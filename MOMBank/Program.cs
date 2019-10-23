a84624dfeb31def9b0debaabe8c68f192bd61562
sarker <{sarker24@gmail.com}>
2019-10-23 23:35:34 +0200

    Add to git

---
+++ MOMBank/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client.Events;
+using System.Threading.Tasks;
+
+namespace MOMBank
+{
+    class Program
+    {
+      
+
+      public static void Main(string[] args)
+        {
+            new Program().RunProgram();
+          
+        }
+        private void RunProgram()
+        {
+            var bankID = Guid.NewGuid();
+            createQueue();
+        }
+        private void createQueue()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+            }
+        }
+
+            private void createoffer(string body)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "offer",
+                                     routingKey:Id,
+                                     basicProperties: null,
+                                     body: body);
+
+            }
+
+            
+        }
+
+      
+
+    }
+
+   
+}
+
---
+++ MOMClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOMClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private void loanInformation(user user)
+        {
+            Console.WriteLine("Loan amount");
+            int amount = int.Parse()(Console.ReadLine());
+            JObject loan = new JObject();
+            loan.add("amount", amount);
+            loan.add("id", id);
+            createQueue(loan.ToString());
+
+        }
+
+        private void createQueue(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "request",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+
+            }
+        }
+    }
+}
